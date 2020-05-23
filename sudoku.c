8af12aef61a536b83faebd4d3a14573c2712625a
maxim-mel <62463135+maxim-mel@users.noreply.github.com>
2020-05-23 16:18:05 +0300

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -60,7 +60,11 @@ void draw_field(){
             if (field[j][i] == 0) {
                 printf("   ");
             } else {
-                printf(" %d ", field[j][i]);
+                if (field[j][i] < 0) {
+                    printf("\x1b[33m");
+                }
+                printf(" %d ", abs(field[j][i]));
+                printf("\x1b[37m");
             }
             if ((j + 1) % 3 == 0) {
                 printf("║");
@@ -96,6 +100,9 @@ void draw_field(){
     if ((y < 1) || (y > 9)) {
         draw_field();
     }
+    if (field[x - 1][y - 1] < 0) {
+        draw_field();
+    }
     printf(" Enter number to write in cell (%d; %d): ", x, y);
     n = scanf("%d", &num);
     fflush(stdin);
@@ -106,11 +113,86 @@ void draw_field(){
         draw_field();
     }
     field[x - 1][y - 1] = num;
-    draw_field();
+    int num_exists_row[9] = {0};
+    int win = 1;
+    for (i = 0; i < 9; i++) {
+        int j;
+        for (j = 0; j < 9; j++) {
+            int c_num = abs(field[j][i]);
+            if (c_num != 0) {
+                num_exists_row[c_num - 1]++;
+            }
+        }
+        int k;
+        for (k = 0; k < 9; k++) {
+            if ((num_exists_row[k] > 1) || (num_exists_row[k] <= 0)) {
+                win = 0;
+                break;
+            }
+        }
+        memset(num_exists_row, 0, sizeof(num_exists_row));
+    }
+    int num_exists_col[9] = {0};
+    for (i = 0; i < 9; i++) {
+        int j;
+        for (j = 0; j < 9; j++) {
+            int c_num = abs(field[i][j]);
+            if (c_num != 0) {
+                num_exists_col[c_num - 1]++;
+            }
+        }
+        int k;
+        for (k = 0; k < 9; k++) {
+            if ((num_exists_col[k] > 1) || (num_exists_col[k] <= 0)) {
+                win = 0;
+                break;
+            }
+        }
+        memset(num_exists_col, 0, sizeof(num_exists_col));
+    }
+    int num_exists_square[9] = {0};
+    for (i = 0; i < 9; i++) {
+        int j;
+        for (j = 0; j < 9; j++) {
+            int x, y;
+            x = (j / 3);
+            y = j - 3*x;
+            int c_num = abs(field[x][y]);
+            if (c_num != 0) {
+                num_exists_square[c_num - 1]++;
+            }
+        }
+        int k;
+        for (k = 0; k < 9; k++) {
+            if ((num_exists_square[k] > 1) || (num_exists_square[k] <= 0)) {
+                win = 0;
+                break;
+            }
+        }
+        memset(num_exists_square, 0, sizeof(num_exists_square));
+    }
+    /*
+    printf("%d ", win);
+    getchar();
+    */
+    /*
+    for (i = 0; i < 9; i++) {
+        printf("%d ", num_exists2[i]);
+    }
+    getchar();
+    */
+    if (win == 1) {
+        printf(" \x1b[92mYou win!\x1b[37m");
+        getchar();
+        main_menu();
+    } else {
+        draw_field();
+    }
 }
 
 void new_game(){
     printf("\nFeature in development...\n");
+    /*
     int i1;
     for (i1 = 0; i1 < 9; i1++) {
         int j2;
@@ -118,6 +200,19 @@ void new_game(){
             field[i1][j2] = 0;
         }
     }
+    */
+    FILE *level_data;
+    char c_num;
+    level_data = fopen("s_level1.txt", "r");
+    int i;
+    for (i = 0; i < 9; i++) {
+        int j;
+        for (j = 0; j < 9; j++) {
+            c_num = fgetc(level_data);
+            field[j][i] = -atoi(&c_num);
+        }
+    }
+    fclose(level_data);
     draw_field();
 }
 
@@ -125,6 +220,7 @@ void load_game(){
     printf("\nFeature in development...\n");
     printf("Press any key...");
     getchar();
+    draw_field();
 }
 
 void set_level(){
